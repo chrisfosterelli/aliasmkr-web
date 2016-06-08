718607cb5cc34c904c5cc3c9879dff010d3f331a
Chris Foster <chris.james.foster@gmail.com>
2016-06-07 21:27:39 -0700

    Add a very basic react app

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+/* Main application entry point
+ */
+
+const React    = require('react')
+const ReactDOM = require('react-dom')
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('main')
+)
